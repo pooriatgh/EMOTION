b517886a9f563c5213ad7d85f7aa1df19239d3be
Pooria <taghizadeh@kahkeshan.com>
2020-10-16 16:16:20 +0330

    - Set trustfulness base on majority of cooperation - runner is modified to show all attributes distribution

--- TDIS_model.py
+++ TDIS_model.py
@@ -2,11 +2,29 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.time import StagedActivation
 import random
-from statistics import mode
+from collections import Counter
 
 
 def assignWithProb(choice1, choice2, prob):
     return choice1 if random.random() < prob else choice2
+
+
+# Function to return majority element present in given list
+def majorityElement(A):
+    # create an empty Hash Map
+    dict = {}
+
+    # store each element's frequency in a dict
+    for i in A:
+        dict[i] = dict.get(i, 0) + 1
+
+    # return the element if its count is more than n/2
+    for key, value in dict.items():
+        if value > len(A) / 2:
+            return key
+
+    # no majority element is present
+    return -1
 
 
 class TDISAgent(Agent):
@@ -36,6 +54,11 @@ class TDISAgent(Agent):
                 self.Score += 1
                 neighborAgentMethod.append('C')
         self.HistoryOfSteps.append(self.Cooperation)
+        mElement = majorityElement(self.HistoryOfSteps)
+        if mElement != -1 and mElement == 'C':
+            self.Trustful = 1
+        else:
+            self.Trustful = -1
 
     def selectNextCooperation(self):
         nextMove = []
@@ -58,12 +81,14 @@ class TDISModel(Model):
         # Create agents
         for i in self.Graph.NodeList:
             cooperationInit = random.choice(['C', 'D'])
-            trustfulInit = random.choice([0, 1])
+            if cooperationInit == 'C':
+                trustfulInit = 1
+            else:
+                trustfulInit = 0
             activeInit = random.choice([0, 1])
             neighborListInit = self.Graph.following(i)
             a = TDISAgent(i, trustfulInit, activeInit, cooperationInit, 2, neighborListInit, self)
             self.schedule.add(a)
 
     def step(self):
-        '''Advance the model by one step.'''
         self.schedule.step()
--- TDIS_runner.py
+++ TDIS_runner.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     all_trust = []
     all_active = []
+    all_coop = []
     all_score = []
     degree_list = []
     # This runs the model 100 times, each model executing 10 steps.
@@ -18,14 +19,14 @@ if __name__ == '__main__':
         # Store the results
         for agent in model.schedule.agents:
             all_trust.append(agent.Trustful)
+            all_coop.append(agent.Cooperation)
             all_active.append(agent.Active)
-            all_score.append(agent.Cooperation)
             degree_list.append(len(agent.NeighborList))
-    # plt.hist(all_trust)
-    # plt.show()
-    # plt.hist(all_active)
-    # plt.show()
-    plt.hist(all_score)
+    plt.hist(all_trust)
+    plt.show()
+    plt.hist(all_coop)
+    plt.show()
+    plt.hist(all_active)
     plt.show()
     plt.hist(degree_list)
     plt.show()
